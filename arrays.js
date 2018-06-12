a98999f3bc1dac4d0722d939b920f111c01281fa
Luoana Chirita <luoana.livadariu@gmail.com>
2018-06-12 11:19:34 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,10 +3,10 @@ var chocolateBars = [
   "hundred grand",
   "kitkat",
   "skittles"
-  ]
+  ];
   
 function addElementToBeginningOfArray(array, item) {
-  
+  return array.push(item);
 }
 
 function destructivelyAddElementToBeginningOfArray(array, item) {
